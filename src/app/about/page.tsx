2b0449a788e8786863646446acaef22fdefdce5e
Lucas Blotta <lucas@blotta.info>
2023-03-19 03:29:19 -0300

    fix eslint error

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,10 +3,7 @@ export default function AboutPage() {
     <div className="flex flex-col h-full justify-center gap-3">
       <h1 className="text-4xl">About Me</h1>
       <p className="text-xl">
-        I am a programmer from São Paulo, Brazil.
-        I'm constantly learning new tecnologies and best practices.
-        I have a thorough knowledge of Linux administration and a drive to become a better programmer.
-        I also enjoy making games and slappin' da bass on my free time.
+        {"I am a programmer from São Paulo, Brazil. I'm constantly learning new tecnologies and best practices. I have a thorough knowledge of Linux administration and a drive to become a better programmer. I also enjoy making games and slappin' da bass on my free time."}
       </p>
     </div>
   )
